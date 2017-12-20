7df778ddd396e065b0e30555fcfa76be4870cb6a
Jeff Woo <cheeseonhead@gmail.com>
2017-12-20 16:51:52 -0500

    Added -i option

--- pull-request.sh
+++ pull-request.sh
@@ -24,5 +24,5 @@ Item         | Status  | Build Status
 ------------ | ------- | ------------
 [IOS-$CUR_BRANCH](https://github.com/cheeseonhead/iosgo/issues/$CUR_BRANCH) | Done    | [![ISSUE-$CUR_BRANCH](https://dashboard.buddybuild.com/api/statusImage?appID=5879f9377457550100e35017&branch=$CUR_BRANCH_LONG&build=latest)](https://dashboard.buddybuild.com/apps/5879f9377457550100e35017/build/latest?branch=$CUR_BRANCH_LONG)" > testfile
 
-hub pull-request -F testFile -b cheeseonhead:$TARGET_BRANCH
+hub pull-request -F testFile -i $CUR_BRANCH -b cheeseonhead:$TARGET_BRANCH
 rm testFile
