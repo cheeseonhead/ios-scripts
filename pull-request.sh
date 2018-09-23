7a8ff9c5656bb8d08a254fe482f90f5d9724b153
Jeffrey Wu <jwu@touchbistro.com>
2018-09-23 15:00:25 -0400

    Updated the pull request script to just have the title for now. The rest is now irrelavent

--- pull-request.sh
+++ pull-request.sh
@@ -19,10 +19,7 @@ TAR_BRANCH_SHORT=$(echo $TARGET_BRANCH | sed -e 's,.*/\(.*\),\1,')
 
 echo -e "Open PR to merge ${CYAN}$CUR_BRANCH${NC} into ${CYAN}$TAR_BRANCH_SHORT${NC}"
 
-echo -e "ISSUE #$CUR_BRANCH → $TAR_BRANCH_SHORT (Close #$CUR_BRANCH)
-Item         | Status  | Build Status
------------- | ------- | ------------
-[IOS-$CUR_BRANCH](https://github.com/cheeseonhead/iosgo/issues/$CUR_BRANCH) | Done    | [![ISSUE-$CUR_BRANCH](https://dashboard.buddybuild.com/api/statusImage?appID=5879f9377457550100e35017&branch=$CUR_BRANCH_LONG&build=latest)](https://dashboard.buddybuild.com/apps/5879f9377457550100e35017/build/latest?branch=$CUR_BRANCH_LONG)" > testfile
+echo -e "ISSUE #$CUR_BRANCH → $TAR_BRANCH_SHORT (Close #$CUR_BRANCH)" > testfile
 
 hub pull-request -F testFile -i $CUR_BRANCH -b cheeseonhead:$TARGET_BRANCH
 rm testFile
